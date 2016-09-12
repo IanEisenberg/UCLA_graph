3b25dd49cfa6b07312f41e4b51cf0cb36812d13f
Ian <IanEisenberg90@gmail.com>
2016-09-12 14:54:54 -0700

    updates

--- Scripts/graph_analysis.py
+++ Scripts/graph_analysis.py
@@ -46,25 +46,72 @@ def plot_mat(mat, labels = []):
     return fig
     
 def print_community_members(G, lookup = {}):
-    assert set(['community','id','within_module_degree','name']) <  set(G.vs.attribute_names()), \
-        "Missing some required vertex attributes. Vertices must have id, name, community and within_module_degree"
+    assert set(['community','id','within_module_degree','name', 'eigen_centrality']) <  set(G.vs.attribute_names()), \
+        "Missing some required vertex attributes. Vertices must have id, name, eigen_centrality, community and within_module_degree"
         
-    print('Key: Node index, Within Module Degree, Measure, Degree')
+    print('Key: Node index, Within Module Degree, Measure, Eigenvector centrality')
     for community in np.unique(G.vs['community']):
         #find members
         members = [lookup.get(v['name'],v['name']) for v in G.vs if v['community'] == community]
         # ids and total degree
         ids = [v['id'] for v in G.vs if v['community'] == community]
-        degrees = [v.degree() for v in G.vs if v['community'] == community]
+        eigen = [round(v['eigen_centrality'],3) for v in G.vs if v['community'] == community]
         #sort by within degree
         within_degrees = [round(v['within_module_degree'],3) for v in G.vs if v['community'] == community]
-        to_print = zip(ids, within_degrees, members, degrees)
+        to_print = zip(ids, within_degrees, members, eigen)
         to_print.sort(key = lambda x: -x[1])
         
         print('Members of community ' + str(community) + ':')
         pprint(to_print)
         print('')
         
+def Graph_Analysis(data, threshold = .3, weight = True):
+    connectivity_matrix = data.corr().as_matrix()
+    # remove diagnoal (required by bct) and uppder triangle
+    np.fill_diagonal(connectivity_matrix,0)
+    
+    #threshold
+    graph_mat = bct.threshold_proportional(connectivity_matrix,threshold)
+    # make a binary version if not weighted
+    if not weight:
+        graph_mat = np.ceil(graph_mat)
+        G = igraph.Graph.Adjacency(graph_mat.tolist(), mode = 'undirected')
+    else:
+        G = igraph.Graph.Weighted_Adjacency(graph_mat.tolist(), mode = 'undirected')
+    layout = G.layout('kk')
+
+    G.vs['id'] = range(len(G.vs))
+    G.vs['name'] = data.columns
+    
+    # community detection
+    # using louvain but also bct.modularity_und which is "Newman's spectral community detection"
+    comm, mod = bct.community_louvain(graph_mat)
+    G.vs['community'] = comm
+    G.vs['within_module_degree'] = bct.module_degree_zscore(graph_mat,comm)
+    G.vs['part_coef'] = bct.participation_coef(graph_mat, comm)
+    if weight:
+        G.vs['eigen_centrality'] = G.eigenvector_centrality(directed = False, weights = G.es['weight'])
+    else:
+        G.vs['eigen_centrality'] = G.eigenvector_centrality(directed = False)
+
+    # plot community structure
+    color_palette = sns.color_palette('hls', np.max(comm))
+    color_dict = {i+1:color_palette[i] for i in range(np.max(comm))}
+    community_color = [color_dict[v] for v in G.vs['community']]
+    visual_style = {'layout': layout, 
+                    'vertex_color': community_color, 
+                    'vertex_size': [c*40+10 for c in G.vs['eigen_centrality']], 
+                    'vertex_label': G.vs['id'],
+                    'vertex_label_size': 20,
+                    'bbox': (1000,1000),
+                    'margin': 50}
+    if weight:
+        visual_style['edge_width'] = [w*4 for w in G.es['weight']]
+        
+    #visualize
+    print_community_members(G, lookup = verbose_lookup)
+    plot_graph(G, visual_style = visual_style, inline = False)
+    return (G, graph_mat)
     
 #**********************************
 # Load Data
@@ -93,90 +140,31 @@ abs_mat = np.abs(corr_mat)
 #**********************************
 # Binary Analysis
 #**********************************
-threshold = .3
-
-#threshold
-thresh_mat = bct.threshold_proportional(corr_mat,threshold)
-# make a binary version
-adj = np.ceil(thresh_mat)
-
-# make a binary graph
-G_bin = igraph.Graph.Adjacency(adj.tolist(), mode = 'undirected')
-G_bin.vs['id'] = range(len(G_bin.vs))
-G_bin.vs['name'] = corr_data.columns
-layout = G_bin.layout('kk')
+G, connectivity_mat = Graph_Analysis(corr_data, weight = False)
 
 # simulate random graphs with same number of nodes and edges
-sim_out = simulate(rep = 10000, fun = lambda: gen_random_graph(n = len(G_bin.vs), m = len(G_bin.es)))
+sim_out = simulate(rep = 10000, fun = lambda: gen_random_graph(n = len(G.vs), m = len(G.es)))
 # get average C and L for random
 C_random = np.mean([i[1] for i in sim_out])
 L_random = np.mean([i[2] for i in sim_out])
 # calculate relative clustering and path length vs random networks
-gamma = G_bin.transitivity_undirected()/C_random
-lam = G_bin.average_path_length()/L_random
+gamma = G.transitivity_undirected()/C_random
+lam = G.average_path_length()/L_random
 # small world coefficient
 sigma = gamma/lam
 
-# community detection
-# using louvain but also bct.modularity_und which is "Newman's spectral community detection"
-comm, mod = bct.community_louvain(adj)
-G_bin.vs['community'] = comm
-within_module_degree = bct.module_degree_zscore(adj,comm)
-G_bin.vs['within_module_degree'] = within_module_degree
-part_coef = bct.participation_coef(adj, comm)
-G_bin.vs['part_coef'] = part_coef
-
-# plot community structure
-color_palette = sns.color_palette('hls', np.max(comm))
-color_dict = {i+1:color_palette[i] for i in range(np.max(comm))}
-community_color = [color_dict[v] for v in G_bin.vs['community']]
-visual_style = {'layout': layout, 
-                'vertex_color': community_color, 
-                'vertex_label': G_bin.vs['id'],
-                'vertex_label_size': 20,
-                'vertex_size': [p*40+10 for p in part_coef],
-                'bbox': (1000,1000),
-                'margin': 50}
-plot_graph(G_bin, visual_style = visual_style)
-print_community_members(G_bin, lookup = verbose_lookup)
-
 # Hubs
 # get "high degree" threshold = 1 std above mean degree
-degree_threshold = np.mean(G_bin.degree()) + np.std(G_bin.degree())
-high_degree_nodes = G_bin.degree() > degree_threshold
-G_bin.vs['hub'] = high_degree_nodes
-connector_hubs = G_bin.vs.select(lambda v: v['hub'] == True and v['part_coef'] > .3)
-provinicial_hubs = G_bin.vs.select(lambda v: v['hub'] == True and v['part_coef'] <= .3)
+degree_threshold = np.mean(G.degree()) + np.std(G.degree())
+high_degree_nodes = G.degree() > degree_threshold
+G.vs['hub'] = high_degree_nodes
+connector_hubs = G.vs.select(lambda v: v['hub'] == True and v['part_coef'] > .3)
+provinicial_hubs = G.vs.select(lambda v: v['hub'] == True and v['part_coef'] <= .3)
 
 #**********************************
 # Weighted Analysis
-#**********************************
-G_weighted = igraph.Graph.Weighted_Adjacency(thresh_mat.tolist(), mode="undirected")
-G_weighted.vs['id'] = range(len(G_weighted.vs))
-G_weighted.vs['name'] = corr_data.columns
-
-# community detection
-# using louvain but also bct.modularity_und which is "Newman's spectral community detection"
-comm, mod = bct.community_louvain(thresh_mat)
-G_weighted.vs['community'] = comm
-within_module_degree = bct.module_degree_zscore(thresh_mat,comm)
-G_weighted.vs['within_module_degree'] = within_module_degree
-part_coef = bct.participation_coef(adj, comm)
-G_weighted.vs['part_coef'] = part_coef
-
-community_color = [color_dict[v] for v in G_weighted.vs['community']]
-
-visual_style = {'layout': layout, 
-                'vertex_color': community_color, 
-                'vertex_size': [p*40+10 for p in part_coef], 
-                'vertex_label': G_weighted.vs['id'],
-                'vertex_label_size': 20,
-                'edge_width': [w*4 for w in G_weighted.es['weight']],
-                'bbox': (1000,1000),
-                'margin': 50}
-plot_graph(G_weighted, visual_style = visual_style, inline = False)
-print_community_members(G_weighted, lookup = verbose_lookup)
-
+#**********************************    
+Graph_Analysis(corr_data, weight = True)
 
 
 
